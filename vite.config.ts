d5659495a30cdafb00f4301338005d4582043de7
Loris Leiva <loris.leiva@gmail.com>
2022-03-07 13:30:58 +0000

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   define: {
     'process.env': process.env ?? {},
   },
   build: {
     target: 'esnext',
   },
-  plugins: [vue()]
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
+  }
 })
