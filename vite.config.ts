efd01755ea61575bfcbb8254ac82222e8b00bd72
Loris Leiva <loris.leiva@gmail.com>
2022-03-22 10:35:12 +0000

    Add node polyfills when building for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,11 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+    rollupOptions: {
+      plugins: [
+        nodePolyfills({ crypto: true }),
+      ],
+    },
   },
   optimizeDeps: {
     esbuildOptions: {
